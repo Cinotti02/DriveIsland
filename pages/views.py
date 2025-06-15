7231811a747f8fe305d2fcff6845a90b950c884b
Francesco Cinotti <129069101+Cinotti02@users.noreply.github.com>
2025-06-15 13:42:22 +0200

    add team management features with dynamic member display and social links

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
+from pages.models import Team
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    team = Team.objects.all()
+    return render(request, 'pages/home.html', {'team': team})
 
 def about(request):
-    return render(request, 'pages/about.html')
+    team = Team.objects.all()
+    return render(request, 'pages/about.html', {'team': team})
 
 def services(request):
     return render(request, 'pages/services.html')
