ed36c7de102b8f26c138163cc6027e120c32e1b4
Francesco Cinotti <129069101+Cinotti02@users.noreply.github.com>
2025-07-03 15:19:49 +0200

    Add car management features including add, edit, and delete functionalities, along with image handling and email notifications for booking cancellations

--- cars/urls.py
+++ cars/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('search/', views.car_search, name='car_search'),
     path('car_details/<int:car_id>', views.car_details, name='car_details'),
     path('<int:car_id>/edit/', views.car_edit, name='edit_car'),
+    path('cars/<int:car_id>/delete/', views.delete_car, name='delete_car'),
+    path('add/', views.add_car, name='add_car'),
 ]
