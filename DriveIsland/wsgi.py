97a9919b56a1367b80f85b2f12ee3fdde42de4c2
Francesco Cinotti <129069101+Cinotti02@users.noreply.github.com>
2025-07-05 11:09:36 +0200

    Aggiunto messaggi di log per migrazioni e caricamento dati in wsgi.py

--- DriveIsland/wsgi.py
+++ DriveIsland/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
+import traceback
 import os
 import django
 from django.core.management import call_command
@@ -17,9 +17,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DriveIsland.settings')
 django.setup()
 application = get_wsgi_application()
 try:
+
     call_command('migrate', interactive=False)
+    print("== MIGRAZIONI OK ==")
     call_command('loaddata', 'backup.json', verbosity=2)
+    print("== BACKUP IMPORTATO CON SUCCESSO ==")
 except Exception as e:
-    import traceback
-    print("=== ERRORE DURANTE IL DEPLOY ===")
+
+    print("=== ERRORE DURANTE IL DEPLOY / LOADDATA ===")
     traceback.print_exc()
